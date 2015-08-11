58515d408d2896efc14a4ccd868f3820f3183fc5
zlahham <zlahham@gmail.com>
2015-08-11 16:32:17 +0100

    HEROKU NOW2

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,14 +1,6 @@
-require "data_mapper"
-require_relative "tag"
-
-# This class corresponds to a table in the database
-# We can use it to manipulate the data
 class Link
-
-  # adds datamapper functionality to this class
   include DataMapper::Resource
 
-  # these property declarations set the column headers in the Link table
   property :id,     Serial # Serial means that it will be auto-incremented for every record
   property :title,  String
   property :url,    String
